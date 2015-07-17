d68fbea653f1124a6bfc9fe5a20a72e20e1bc84b
archafell <hastin@163.com>
2015-07-17 09:16:35 +0800

    modify servlet

--- src/java/org/oursight/study/javaee/servlet/SimpleServlet.java
+++ src/java/org/oursight/study/javaee/servlet/SimpleServlet.java
@@ -9,6 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 
 public class SimpleServlet extends HttpServlet {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
 		System.out.println(new java.sql.Time(System.currentTimeMillis()));
