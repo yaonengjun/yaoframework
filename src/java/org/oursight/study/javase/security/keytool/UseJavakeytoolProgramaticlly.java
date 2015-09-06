51be84f80b81ce809d4421c11bfbba3b2cce1b06
nengjun.yao@oracle.com <nengjun.yao@oracle.com>
2015-09-06 16:53:53 +0800

    add keystore sample code

---
+++ src/java/org/oursight/study/javase/security/keytool/UseJavakeytoolProgramaticlly.java
@@ -0,0 +1,33 @@
+package org.oursight.study.javase.security.keytool;
+
+import java.io.FileOutputStream;
+import java.security.KeyStore;
+
+public class UseJavakeytoolProgramaticlly {
+	
+	/**
+	 * 创建一个KeyStore，生成咋ixiangm的根目录中
+	 * @throws Exception
+	 * @author neyao,2015年9月6日 下午4:49:26
+	 */
+	public static void createKeyStore() throws Exception {
+		KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+
+		char[] password = "111111".toCharArray();
+		ks.load(null, password);
+
+		// Store away the keystore.
+		FileOutputStream fos = new FileOutputStream("MyTestKeyStoreFile");
+		ks.store(fos, password);
+		fos.close();
+	}
+	
+	public static void main(String[] args) {
+		try {
+			createKeyStore();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
